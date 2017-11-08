3245b07fa2a7a25ea335368c9b8c9d2a60f75685
harry <harry.maierhofer@gmail.com>
2017-11-08 15:18:30 +0100

    Show multiple fav entries in entity sentiment report

--- src/main/java/harry/Report.java
+++ src/main/java/harry/Report.java
@@ -6,17 +6,17 @@ import harry.DerbyDB;
 public class Report {
 	public String entitySentiment() {
 		String report = "This will be the report.";
-		String fav = "";
-		String worst = "";
+		String[] fav = new String[3];
+		String[] worst  = new String[3];
 		DerbyDB db = new DerbyDB();
 		try {
-			worst = db.getEntitySentimentData("ASC");
-			fav = db.getEntitySentimentData("DESC");
+			worst = db.getEntitySentimentData("ASC", 3);
+			fav = db.getEntitySentimentData("DESC", 3);
 		} catch (SQLException sqle) {
       System.out.println(sqle.toString());
     }
 
-    report = "You like "+fav+". You dislike "+worst+".";
+    report = "You like "+fav[0]+", "+fav[1]+" and "+fav[2]+". You dislike "+worst[0]+", "+worst[1]+" and "+worst[2]+".";
 
 		return report;
 	}
