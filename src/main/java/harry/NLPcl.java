c19937927611159aa2599c9bfa8afb336bd780e6
ATN_89 <ATN_89@SZFEDV.szf.at>
2017-11-10 10:20:46 +0100

    Create reports for a specific user from cl

--- src/main/java/harry/NLPcl.java
+++ src/main/java/harry/NLPcl.java
@@ -2,12 +2,23 @@ package harry;
 
 import harry.DerbyDB;
 import java.sql.SQLException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 public class NLPcl {
+
+  @Parameter(names = "-user", description = "telegram @ handle (without the @)")
+  private String user;
+
   public static void main(String... args) {
+
+  	NLPcl nlpcl = new NLPcl();
+    JCommander.newBuilder()
+    	.addObject(nlpcl)
+        .build()
+        .parse(args);
   
     //Initialize the database
-  	//DerbyDB db = new DerbyDB();
   	DerbyDB db = new DerbyDB();
   	try {
   		db.checkDB();
@@ -16,7 +27,12 @@ public class NLPcl {
   		sqle.printStackTrace();
   	}
 
-	Report report = new Report();
-    System.out.println(report.entitySentiment("jolieblonde"));
+	nlpcl.report();
+  }
+
+  public void report() {
+
+  	Report report = new Report();
+	System.out.println(report.entitySentiment(user));
   }
 }
